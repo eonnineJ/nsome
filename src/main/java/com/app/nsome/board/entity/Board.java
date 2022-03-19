df4cacbba2851ce8d5987bf8e0e0a8195cc0699b
hichoidev <hichoidev@gmail.com>
2022-03-19 18:53:07 +0900

    Add Board
    entity - Board

---
+++ src/main/java/com/app/nsome/board/entity/Board.java
@@ -0,0 +1,32 @@
+package com.app.nsome.board.entity;
+
+import com.app.nsome.member.entity.Member;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    Member writerId;
+
+    @ManyToOne
+    @JoinColumn(name = "name")
+    Member writer;
+
+    LocalDate writeDate;
+
+    String title;
+
+    String contant;
+
+    Integer hits;
+
+}
