0fcbb13034ae583e60866764777396efcc794a17
eonnine <fnsldkzbfl@naver.com>
2022-03-19 13:44:20 +0900

    First commit

---
+++ src/main/java/com/app/nsome/NsomeApplication.java
@@ -0,0 +1,13 @@
+package com.app.nsome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NsomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/nsome/NsomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.nsome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
